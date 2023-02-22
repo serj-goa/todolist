edf2312458ba30a0b18bc042075d18d213103748
Serhii Borodin <69416078+serj-goa@users.noreply.github.com>
2023-02-22 20:03:11 +0300

    Add custom user model

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', )
+    list_filter = ('is_staff', 'is_active', 'is_superuser', )
+    search_fields = ('email', 'username', 'first_name', 'last_name', )
+    exclude = ('password', )
+    readonly_fields = ('last_login', 'date_joined', )
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    ordering = ('email', )
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+
+        return super().get_fieldsets(request, obj)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('email', )
+
+        return self.readonly_fields
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        if 'password' in form.changed_data:
+            obj.set_password(form.cleaned_data['password'])
+            obj.save()
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
