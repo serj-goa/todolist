6f1abebc5b5d69ace5980501751d082e064ca794
Serhii Borodin <69416078+serj-goa@users.noreply.github.com>
2023-03-29 11:29:10 +0300

    Add board

--- todolist/goals/models/__init__.py
+++ todolist/goals/models/__init__.py
@@ -1,2 +1,3 @@
 from goals.models.base_model import BaseModel
+from goals.models.board import Board, BoardParticipant
 from goals.models.goal import Goal, GoalCategory, GoalComment
--- todolist/goals/models/goal.py
+++ todolist/goals/models/goal.py
@@ -1,7 +1,7 @@
-from django.db import models
+from django.db import models as m
 
 from core.models import CustomUser
-from goals.models import BaseModel
+from goals.models import BaseModel, Board
 
 
 class Goal(BaseModel):
@@ -9,60 +9,48 @@ class Goal(BaseModel):
         verbose_name = 'Цель'
         verbose_name_plural = 'Цели'
 
-    class Status(models.IntegerChoices):
+    class Status(m.IntegerChoices):
         to_do = 1, 'К выполнению'
         in_progress = 2, 'В процессе'
         done = 3, 'Выполнено'
         archived = 4, 'Архив'
 
-    class Priority(models.IntegerChoices):
+    class Priority(m.IntegerChoices):
         low = 1, 'Низкий'
         medium = 2, 'Средний'
         high = 3, 'Высокий'
         critical = 4, 'Критический'
 
-    title = models.CharField(verbose_name='Название', max_length=255)
-    description = models.TextField(verbose_name='Описание', null=True, blank=True)
-    category = models.ForeignKey(
-        to='GoalCategory',
-        verbose_name='Категория',
-        on_delete=models.CASCADE,
-        related_name='goals'
-    )
-    status = models.PositiveSmallIntegerField(
-        verbose_name='Статус',
-        choices=Status.choices,
-        default=Status.to_do,
-    )
-    priority = models.PositiveSmallIntegerField(
-        verbose_name='Приоритет',
-        choices=Priority.choices,
-        default=Priority.medium,
-    )
-    due_date = models.DateTimeField(verbose_name='Дата выполнения', null=True, blank=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT, verbose_name='Автор', related_name='goals')
+    title = m.CharField(verbose_name='Название', max_length=255)
+    description = m.TextField(verbose_name='Описание', null=True, blank=True)
+    category = m.ForeignKey(to='GoalCategory', verbose_name='Категория', on_delete=m.CASCADE, related_name='goals')
+    status = m.PositiveSmallIntegerField(verbose_name='Статус', choices=Status.choices, default=Status.to_do)
+    priority = m.PositiveSmallIntegerField(verbose_name='Приоритет', choices=Priority.choices, default=Priority.medium)
+    due_date = m.DateTimeField(verbose_name='Дата выполнения', null=True, blank=True)
+    user = m.ForeignKey(CustomUser, on_delete=m.PROTECT, verbose_name='Автор', related_name='goals')
 
     def __str__(self):
         return self.title
 
 
 class GoalCategory(BaseModel):
+    title = m.CharField(verbose_name='Название', max_length=255)
+    user = m.ForeignKey(CustomUser, verbose_name='Автор', on_delete=m.PROTECT)
+    is_deleted = m.BooleanField(verbose_name='Удалена', default=False)
+    board = m.ForeignKey(Board, verbose_name='Доска', on_delete=m.PROTECT, related_name='categories')
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-
-    title = models.CharField(verbose_name='Название', max_length=255)
-    user = models.ForeignKey(CustomUser, verbose_name='Автор', on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(verbose_name='Удалена', default=False)
 
     def __str__(self):
         return self.title
 
 
 class GoalComment(BaseModel):
-    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT, verbose_name='Автор', related_name='comments')
-    goal = models.ForeignKey(Goal, verbose_name='Цель', on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField(verbose_name='Текст')
+    user = m.ForeignKey(CustomUser, on_delete=m.PROTECT, verbose_name='Автор', related_name='comments')
+    goal = m.ForeignKey(Goal, verbose_name='Цель', on_delete=m.CASCADE, related_name='comments')
+    text = m.TextField(verbose_name='Текст')
 
     class Meta:
         verbose_name = 'Комментарий'
